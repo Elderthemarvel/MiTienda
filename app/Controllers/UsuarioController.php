165c6212dfa948be09c28d45af75a2a5791a7221
LuisGabriel8615 <luis.gabriel.200615485@gmail.com>
2024-11-06 00:07:16 -0600

    Update UsuarioController.php
    función traslado a formulario nuevo usuario

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -16,5 +16,15 @@ class UsuarioController extends Controller
         return view ('administrador/usuarios',$datosusuario);
     }
    
-    
+    public function nuevousuario()
+    {
+        //$usuarios = new UsuariosModel();
+        //$datosusuario['perfil'] = $this->perfil;
+        //$datosusuario['datosusuario']=$usuarios->findAll();
+        //$datosusuario['perfil'] = 1;
+
+        //print_r($datosusuario);
+        $datosusuario['perfil'] = 1;
+        return view ('administrador/nuevo_user',$datosusuario);
+    }
 }
