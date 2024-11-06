b96b47dd4913abb09746f9219919d6ca11b30b03
LuisGabriel8615 <luis.gabriel.200615485@gmail.com>
2024-11-05 23:46:39 -0600

    Update UsuarioController.php
    Probando nuevo usuario

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -8,9 +8,12 @@ class UsuarioController extends Controller
     public function verusuarios()
     {
         $usuarios = new UsuariosModel();
-        $datosusuario['datosusuario']=$usuarios->findAll();
         //$datosusuario['perfil'] = $this->perfil;
-        return view ('administrador/usuarios',$datosusuario);
+        $datosusuario['datosusuario']=$usuarios->findAll();
+        $datosusuario['perfil'] = 1;
+
+        print_r($datosusuario);
+        return view ('administrador/nuevo_user',$datosusuario);
     }
    
     
