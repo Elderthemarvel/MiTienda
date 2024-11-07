6aa2ab2a0f5176ddd43b5f9f85e72099efffeb04
LuisGabriel8615 <luis.gabriel.200615485@gmail.com>
2024-11-06 23:24:06 -0600

    Update UsuarioController.php
    Eliminando mensajes

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -23,12 +23,6 @@ class UsuarioController extends Controller
    
     public function nuevousuario()
     {
-        //$usuarios = new UsuariosModel();
-        //$datosusuario['perfil'] = $this->perfil;
-        //$datosusuario['datosusuario']=$usuarios->findAll();
-        //$datosusuario['perfil'] = 1;
-
-        //print_r($datosusuario);
         $datosusuario['perfil'] = 1;
         return view ('administrador/nuevo_user',$datosusuario);
     }
