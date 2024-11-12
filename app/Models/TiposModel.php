464f31d435da6efff475381ac1a4efc7ab6f561c
LuisGabriel8615 <luis.gabriel.200615485@gmail.com>
2024-11-11 22:52:05 -0600

    Create TiposModel.php
    Creando modelo para tipos de usuario

---
+++ app/Models/TiposModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TiposModel extends Model
+{
+    protected $table            = 'tipos';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = false;
+    protected $allowedFields    = ['nom_tipo', 'created_at', 'updated_at', 'deleted_at'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
